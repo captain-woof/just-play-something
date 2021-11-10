44343c12df13a101124f2ceda752acd9c07d923f
Sohail Saha <captainwoof.official@gmail.com>
2021-11-10 12:23:58 +0530

    Replaced apostrophe by its HTML entity

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.css'
 const Header = () => {
     return (
         <header className={styles.header_wrapper}>
-            <h1 className={styles.header_text}>Just play somethin'</h1>
+            <h1 className={styles.header_text}>Just play somethin&apos;</h1>
             <p className={styles.header_subtitle}>Discover new tracks, everyday.</p>
         </header>
     )
