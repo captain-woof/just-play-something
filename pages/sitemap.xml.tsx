958689fc7c88d04da0b123632cfc2797de4f714e
Sohail Saha <captainwoof.official@gmail.com>
2021-11-11 18:23:15 +0530

    Added dynamic sitemap

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,42 @@
+import { GetServerSidePropsContext } from 'next'
+
+export const getServerSideProps = async ({ res }: GetServerSidePropsContext) => {
+  // Get base url
+  const origin = process.env.NEXT_PUBLIC_APP_ORIGIN
+
+  // Initializing pages with static pages
+  const pages = [
+    `${origin}`,
+    `${origin}/about`
+  ]
+
+  // Making the sitemap
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages
+      .map((url) => {
+        return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>daily</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+      })
+      .join("")}
+    </urlset>
+  `;
+
+  // Returning sitemap
+  res.setHeader("Content-Type", "text/xml")
+  res.write(sitemap)
+  res.end()
+
+  return {
+    props: {}
+  }
+}
+
+// Blank, because no components must be rendered
+export default function Sitemap() { }
