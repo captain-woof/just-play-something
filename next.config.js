1bf635a675abf1c1bbe47b06ab51d8091781a2d5
Sohail Saha <captainwoof.official@gmail.com>
2021-11-11 19:47:25 +0530

    Fixed PWA

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
-
+const withPWA = require("next-pwa");
 module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['usercontent.jamendo.com']
   },
   pwa: {
-    dest: 'public',
-    disable: false,
-    scope: '/',
-    skipWaiting: true
-  }
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+    buildExcludes: [/middleware-manifest\.json$/]
+  },
 })
